fd9c72a0045f1cd1b37a968233363829f0bcf44f
Rajesh Nadiminti <rajeshnadiminti_9@hotmail.com>
2018-06-28 16:32:53 -0400

    Update mfc.rb

--- simple_multicast/mfc.rb
+++ simple_multicast/mfc.rb
@@ -1,24 +1,3 @@
-#
-# Multicast Forwarding Cache (MFC)
-#
-# Author: Kazuya Suzuki
-#
-# Copyright (C) 2012 NEC Corporation
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License, version 2, as
-# published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
-
 
 require "set"
 
@@ -48,10 +27,3 @@ class MFC
     @db[ group.to_i ]
   end
 end
-
-
-### Local variables:
-### mode: Ruby
-### coding: utf-8
-### indent-tabs-mode: nil
-### End:
